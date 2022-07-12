4cd107c978809f390829c57ac31d20da93b53d31
SaitoMokichi615 <gyro0615@gmail.com>
2022-07-12 15:01:52 +0900

    ユーザ基本情報管理画面作成

---
+++ packageBase/js/accordion.js
@@ -0,0 +1,19 @@
+// 厳密なエラーチェックをする
+'use strict';
+{
+  const dts = document.querySelectorAll('dt');
+
+  dts.forEach(dt => {
+    dt.addEventListener("click", () =>{
+      
+      dt.parentNode.classList.toggle('appear');
+      
+      dts.forEach(el => {
+        if(dt !== el){
+          el.parentNode.classList.remove("appear");
+        }
+      });
+
+    });
+  });
+}
